f63d28c6d7096c4dfb8f6f8ac9944d92b4d22762
acat22 <absitesmail@gmail.com>
2015-04-20 22:16:28 +0300

    'first'

---
+++ jquery.Jchar.js
@@ -0,0 +1,75 @@
+(function( $ ) {
+	
+	function CharTyped(obj, callback) {
+		var o = {
+			txtSnapshot : {
+				prtxt : '',
+				sel : {}
+			},
+			obj : null,
+			onKeyPressed : function(event) {
+				var yes = 1;
+				if (event.keyCode == 13) yes = 0;
+				var pos = this.obj.selectionStart;
+				var posEnd = this.obj.selectionEnd;
+				var str = this.obj.value;
+				if (this.txtSnapshot.sel.s !== undefined) {
+					if ((this.txtSnapshot.sel.s + 1 + (this.txtSnapshot.prtxt.length - this.txtSnapshot.sel.e)) != str.length) yes = 0;
+				} else {
+					if (str == this.txtSnapshot.prtxt) yes = 0;
+					if (str.length < this.txtSnapshot.prtxt.length) yes = 0;
+				}
+				var ch = str.charAt(pos - 1);
+				this.txtSnapshot.prtxt = str;
+				this.txtSnapshot.sel = {};
+				if (yes) this.callback(ch);
+			},
+			off : function() {
+				this.on(this.obj, this.callback, true);
+			},
+			on : function(obj, callback, rem) {
+				var o = this;
+				if (!this.cb) this.cb = function (event) {
+					event = event || window.event;
+					o.onKeyPressed(event);
+				}
+				if (rem) {
+					if (window.removeEventListener){
+						this.obj.removeEventListener("keyup", this.cb, false);
+					} else {
+						this.obj.detachEvent("onkeyup", this.cb);
+					}
+					this.obj = null;
+					this.callback = null;
+					return;
+				}
+				this.obj = obj;
+				this.callback = callback;
+				if (window.addEventListener){
+					this.obj.addEventListener("keyup", this.cb, false);
+				} else {
+					this.obj.attachEvent("onkeyup", this.cb);
+				}
+			}
+		};
+		if (obj && callback) o.on(obj, callback);
+		return o;
+	}
+
+    $.fn.onchar = function(callback) {
+		
+		var o = this.data('_chartyped');
+		if (!callback) {
+			if (o) o.off();
+			this.data('_chartyped', null);
+			o = null;
+		} else
+		if (!o) {
+			o = new CharTyped(this.get(0), callback);
+			this.data('_chartyped', o);
+		}
+        return this;
+ 
+    };
+ 
+}( jQuery ));
